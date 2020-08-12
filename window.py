e65705e757145f0a58c05cee3c5b994e56b5ee5b
animolopez <eguchi.hikaru.kyushu@gmail.com>
2020-08-13 05:51:25 +0900

    add butterworth filterling

--- window.py
+++ window.py
@@ -1,13 +1,14 @@
 import numpy as np
+from scipy import signal
 
-def sqrtCosEnv(length,rft=(0.005),frs=(44100)):
+def sqrtCosEnv(length,rft=(0.005),fs=(44100)):
     """
-    rft    ：rise and fall time [s]
+    rft    : rise and fall time [s]
     length : total length of window [s]
-    frs    : sampling freq [Hz]
+    fs    : sampling freq [Hz]
     """
-    rfsamp = int(np.round(rft * frs))
-    windowsamp = int(np.round(length * frs))
+    rfsamp = int(np.round(rft * fs))
+    windowsamp = int(np.round(length * fs))
     flatsamp = windowsamp - (2 * rfsamp)
     time_index = np.arange(0, 1, 1 / rfsamp)
 
@@ -18,14 +19,14 @@ def sqrtCosEnv(length,rft=(0.005),frs=(44100)):
 
     return env
 
-def CosEnv(length,rft=(0.005),frs=(44100)):
+def CosEnv(length,rft=(0.005),fs=(44100)):
     """
-    rft    ：rise and fall time [s]
+    rft    : rise and fall time [s]
     length : total length of window [s]
-    frs    : sampling freq [Hz]
+    fs    : sampling freq [Hz]
     """
-    rfsamp = int(np.round(rft * frs))
-    windowsamp = int(np.round(length * frs))
+    rfsamp = int(np.round(rft * fs))
+    windowsamp = int(np.round(length * fs))
     flatsamp = windowsamp - (2 * rfsamp)
     time_index = np.arange(0, 1, 1 / rfsamp)
 
@@ -35,3 +36,62 @@ def CosEnv(length,rft=(0.005),frs=(44100)):
     env = np.concatenate((r_env, flat_env, f_env), 0)
 
     return env
+
+def ButterworthBP(x,lowcut,highcut,fs,order=(None)):
+    """
+    x       : input signal
+    lowcut  : Lowcut frequency [Hz]
+    highcut : Highcut frequency [Hz]
+    fs      : Sampling frequency [Hz]
+    order   : Order of the butterworth bandpath filter
+    """
+    if order == None:
+        wslow = lowcut * 0.5
+        wshigh = highcut * 2
+        order, wn = signal.buttord(wp=[lowcut,highcut], ws=[wslow,wshigh], gpass=3, gstop=80, fs=fs)
+        print('order: %d' % order)
+    else:
+        wn = [lowcut, highcut]
+
+    sos = signal.butter(order, wn, btype='bandpass', output='sos', fs=fs)
+    y = signal.sosfilt(sos, x)
+
+    return y
+
+def ButterworthLP(x,cutoff,fs,order=(None)):
+    """
+    x       : input signal
+    cutoff  : cutoff frequency [Hz]
+    fs      : Sampling frequency [Hz]
+    order   : Order of the butterworth bandpath filter
+    """
+    if order == None:
+        ws = cutoff * 2
+        order, wn = signal.buttord(wp=cutoff, ws=ws, gpass=3, gstop=80, fs=fs)
+        print('order: %d' % order)
+    else:
+        wn = cutoff
+
+    sos = signal.butter(order, wn, btype='lowpass', output='sos', fs=fs)
+    y = signal.sosfilt(sos, x)
+
+    return y
+
+def ButterworthHP(x,cutoff,fs,order=(None)):
+    """
+    x       : input signal
+    cutoff  : cutoff frequency [Hz]
+    fs      : Sampling frequency [Hz]
+    order   : Order of the butterworth bandpath filter
+    """
+    if order == None:
+        ws = cutoff * 0.5
+        order, wn = signal.buttord(wp=cutoff, ws=ws, gpass=3, gstop=80, fs=fs)
+        print('order: %d' % order)
+    else:
+        wn = cutoff
+
+    sos = signal.butter(order, wn, btype='highpass', output='sos', fs=fs)
+    y = signal.sosfilt(sos, x)
+
+    return y
